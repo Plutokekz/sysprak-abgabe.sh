51193aaab57190cf53e4018fa1d556ed9f9bc77f
Heinrich Lindner <koli@DESKTOP-OVAVI83.localdomain>
2020-12-31 17:49:07 +0100

    Created gamePhase file. Mostly stubs, requires functioning SHM and is also part of tasks 2 and 3 in Milestone 3.

---
+++ gamePhase.h
@@ -0,0 +1,3 @@
+
+// For communication between thinker and connector this method might need the pipe ID and thinker process ID for signal. For testing purposes this has not been implemented yet, but should be expanded later.
+void gamePhase();
--- performConnection.h
+++ performConnection.h
@@ -9,12 +9,17 @@ typedef struct Opt
     char playerId[2];
 } opt_t;
 
-typedef enum COMMAND { VERSION, ID, PLAYER } COMMAND;
+// Commands that should be sendable to the server
+typedef enum COMMAND { VERSION, ID, PLAYER, OKWAIT, THINKING, MOVEOK, PLAY } COMMAND;
 
 void performConnection(int sock, opt_t *opt);
 
+// Sends command from Enum with value string after to the server. Value can also be an empty string, but must not be a nullpointer (WIP)
 void sendCommand(COMMAND c, char *value);
 
+// Returns address of buffer with complete command sent by the server.
+// Returns everything, including "+ " and newline characters
+// Very rarely breaks, depending on how server sends commands (WIP)
 char *recvCommand();
 
 #endif
