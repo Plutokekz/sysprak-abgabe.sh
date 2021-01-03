033f8f495e454b0e6573ab8a34d0801208760c55
Erik Kellenter <erikkellenter@mailbox.org>
2021-01-03 20:59:11 +0100

    major improvements to recvCommand and the implementation of parsing and priting the server response

--- performConnection.c
+++ performConnection.c
@@ -21,24 +21,165 @@
 int SOCK;
 char commandBuff[RECV_BUFF_SIZE];
 
-void printServerResponse(char *recvBuff, char *command) {
-  // char outBuff[BUFF_SIZE];
-
-  if (command == NULL) {
-    printf("S: %s", recvBuff);
+char *lookup(COMMAND c) {
+  char *command;
+  switch (c) {
+  case VERSION:
+    command = "VERSION";
+    break;
+  case ID:
+    command = "ID";
+    break;
+  case PLAYER:
+    command = "PLAYER";
+    break;
+  default:
+    printf("lookup COMMAND failed\n");
+    exit(EXIT_FAILURE);
   }
-  // else if (strcmp(command, "PLAYER") == 0) {
-  //  int playerId;
-  //  int total;
-  //  char playerName[BUFF_SIZE];
-  //  sscanf(recvBuff, "+ YOU %i %[^\n]\n+ TOTAL %i", &playerId, playerName,
-  //         &total);
-  //  printf("playerId: %i\n", playerId);
-  //  printf("total: %i\n", total);
-  //  printf("playerName: %s\n", playerName);
-  //
-  //  printf("S: + YOU ARE NOW %s")
-  //}
+  return command;
+}
+
+// prefix nullable
+void rawAppend(game_info *gameInfo, char *recvBuff) {
+  static char *prolBuff = NULL;
+  static size_t size = 0;
+  size_t len = strlen(recvBuff);
+  if (size == 0) {
+    prolBuff = malloc(len + 1);
+    strcpy(prolBuff, recvBuff);
+  } else {
+    prolBuff = realloc(prolBuff, size + len + 1);
+    strcat(prolBuff + size, recvBuff);
+  }
+  size += len;
+  gameInfo->raw = prolBuff;
+}
+
+// parse and frees
+// expects null terminated string
+game_info *parseCommand(char *recvBuff, COMMAND c) {
+  static game_info gameInfo;
+  // static player_t **playerList = NULL;
+  if (recvBuff == NULL || c == RETURN) {
+    return &gameInfo;
+  }
+  rawAppend(&gameInfo, recvBuff);
+  int rv;
+  switch (c) {
+  case START: {
+    char *serverVersionBuff = calloc(strlen(recvBuff), 1);
+    size_t maxServerVersionSize = sizeof(gameInfo.serverVersion);
+    if ((rv = sscanf(recvBuff, "%*[^0-9]%s", serverVersionBuff)) > 0 &&
+        strlen(serverVersionBuff) < maxServerVersionSize) {
+      strcpy(gameInfo.serverVersion, serverVersionBuff);
+    }
+    free(serverVersionBuff);
+  } break;
+  case VERSION:
+    break;
+  case ID: {
+    size_t maxGamekindNameSize = sizeof(gameInfo.gamekindName);
+    size_t maxGameNameSize = sizeof(gameInfo.gameName);
+    char *gamekindNameBuff = calloc(strlen(recvBuff), 1);
+    char *gameNameBuff = calloc(strlen(recvBuff), 1);
+    if ((rv = sscanf(recvBuff, "+ PLAYING %[^\n]\n+ %[^\n]", gamekindNameBuff,
+                     gameNameBuff)) > 0 &&
+        strlen(gamekindNameBuff) < maxGamekindNameSize &&
+        strlen(gameNameBuff) < maxGameNameSize) {
+      strcpy(gameInfo.gamekindName, gamekindNameBuff);
+      strcpy(gameInfo.gameName, gameNameBuff);
+    }
+    free(gamekindNameBuff);
+    free(gameNameBuff);
+  } break;
+  case PLAYER: {
+    int n;
+    player_t player;
+    player.ready = 1;
+    size_t maxPlayerNameSize = sizeof(player.playerName);
+    char *playerNameBuff = calloc(strlen(recvBuff), 1);
+    char *rest = calloc(strlen(recvBuff), 1);
+    if ((rv = sscanf(recvBuff, "+ YOU %u %[^\n]\n+ TOTAL %u\n%n",
+                     &player.playerId, playerNameBuff, &gameInfo.total, &n)) >
+            0 &&
+        strlen(playerNameBuff) < maxPlayerNameSize) {
+      strcpy(player.playerName, playerNameBuff);
+      // own player initialized, continue with player list
+      gameInfo.playerList = malloc(gameInfo.total * sizeof(player_t *));
+      gameInfo.playerList[0] = malloc(sizeof(player_t));
+      memcpy(gameInfo.playerList[0], &player, sizeof(player_t));
+      for (unsigned int i = 1; i < gameInfo.total; i++) {
+        gameInfo.playerList[i] = malloc(sizeof(player_t));
+        int tmpN;
+        if ((rv = sscanf(recvBuff + n, "+ %u %[^\n]\n%n", &player.playerId,
+                         playerNameBuff, &tmpN)) > 0) {
+          size_t playerNameSize = strlen(playerNameBuff);
+          if (playerNameSize < maxPlayerNameSize) {
+            // playerName = <<Player-Name>> <<Ready>>
+            playerNameBuff[playerNameSize - 2] = '\0';
+            player.ready = playerNameBuff[playerNameSize - 1];
+            strcpy(player.playerName, playerNameBuff);
+            memcpy(gameInfo.playerList[i], &player, sizeof(player_t));
+          }
+        }
+        n += tmpN;
+      }
+    }
+    free(playerNameBuff);
+    free(rest);
+    break;
+  }
+  default:
+    printf("command not supported\n");
+    exit(EXIT_FAILURE);
+    break;
+  }
+  if (rv == -1) {
+    perror("parsing commands failed\n");
+    exit(EXIT_FAILURE);
+  }
+  return &gameInfo;
+}
+
+// helper
+game_info *getGameInfo() { return parseCommand(NULL, RETURN); }
+
+void freeGameInfo(game_info *gameInfo) {
+  if (gameInfo != NULL) {
+    free(gameInfo->raw);
+    for (unsigned int i = 0; i < gameInfo->total; i++) {
+      free(gameInfo->playerList[i]);
+    }
+    free(gameInfo->playerList);
+  }
+}
+
+void printProlog(game_info *gameInfo, P_FLAG f) {
+  switch (f) {
+  case PRETTY:
+    printf("Your are connected with MNM Gameserver v%s\n",
+           gameInfo->serverVersion);
+    printf("In %s %s:\n", gameInfo->gamekindName, gameInfo->gameName);
+    for (unsigned int i = 0; i < gameInfo->total; i++) {
+      if (gameInfo->playerList[i]->ready) {
+        printf("Player %u (%s) is ready\n", gameInfo->playerList[i]->playerId + 1,
+               gameInfo->playerList[i]->playerName);
+      } else {
+        printf("Player %u (%s) is not ready\n",
+               gameInfo->playerList[i]->playerId + 1,
+               gameInfo->playerList[i]->playerName);
+      }
+    }
+    break;
+  case DEBUG:
+    printf("%s", gameInfo->raw);
+    break;
+  default:
+    printf("wrong flag\n");
+    exit(EXIT_FAILURE);
+    break;
+  }
 }
 
 int charCount(char *s, int n) {
@@ -49,7 +190,7 @@ int charCount(char *s, int n) {
   return count;
 }
 
-char *recvCommand(int lines) {
+void *recvCommand(int lines) {
   int lineCount = lines;
   if (lines == 0) {
     lineCount = INT_MAX;
@@ -74,17 +215,16 @@ char *recvCommand(int lines) {
       free(buff);
       exit(EXIT_FAILURE);
     default:
-      printf("bytes received: %li\n", tmp);
       if (buff[0] == '-') {
-        printServerResponse(buff, NULL);
+        buff[bytes] = '\0';
+        printf("%s", buff);
         free(buff);
         exit(EXIT_FAILURE);
       }
       lineCount -= charCount(buff + bytes, tmp);
       bytes += tmp; // increment returned bytes every loop (at least 1 byte)
       char *end;
-      if (lineCount > lines &&
-          (end = strstr(buff, "END")) != NULL) {
+      if (lineCount > lines && (end = strstr(buff, "END")) != NULL) {
         end = strchr(end, '\n');
         buff[bytes] = '\0';
         memset(commandBuff, 0, RECV_BUFF_SIZE);
@@ -98,44 +238,21 @@ char *recvCommand(int lines) {
     }
   }
   buff[bytes] = '\0';
-  return buff;
-}
-
-char *lookup(COMMAND c) {
-  char *command;
-  switch (c) {
-  case VERSION:
-    command = "VERSION";
-    break;
-  case ID:
-    command = "ID";
-    break;
-  case PLAYER:
-    command = "PLAYER";
-    break;
-  default:
-    printf("lookup COMMAND failed\n");
-    exit(EXIT_FAILURE);
-  }
-  return command;
+  return (void *)buff;
 }
 
 // supports VERSION, ID, PLAYER
 void sendCommand(COMMAND c, char *value) {
   char sendBuff[50] = "";
   char *command = lookup(c);
-
   size_t size;
   if (value == NULL || strlen(value) == 0) {
     size = sprintf(sendBuff, "%s\n", command);
   } else {
     size = sprintf(sendBuff, "%s %s\n", command, value);
   }
-
-  if (send(SOCK, sendBuff, size, 0) > 0) {
-    printf("C: %s", sendBuff);
-  } else {
-    printf("sending failed\n");
+  if (send(SOCK, sendBuff, size, 0) < 0) {
+    perror("sending failed\n");
     exit(EXIT_FAILURE);
   }
 }
@@ -167,28 +284,33 @@ void performConnection(int sock, opt_t *opt) {
   SOCK = sock;
   setupConnection(sock);
 
-  char *recvBuff;
-
+  void *recvBuff;
   recvBuff = recvCommand(1); // + MNM Gameserver <<Gameserver Version>>
                              // accepting connections
-  printServerResponse(recvBuff, NULL);
+  parseCommand(recvBuff, START);
+  free(recvBuff);
 
   sendCommand(VERSION, CLIENT_VERSION);
 
   recvBuff =
       recvCommand(1); // + Client version accepted - please send Game-ID to join
-  printServerResponse(recvBuff, NULL);
+  parseCommand(recvBuff, VERSION);
+  free(recvBuff);
 
   sendCommand(ID, opt->gameId);
 
   recvBuff = recvCommand(2); // + PLAYING <<Gamekind-Name>>\n + <<Game-Name>>
-  printServerResponse(recvBuff, NULL);
+  parseCommand(recvBuff, ID);
+  free(recvBuff);
 
   // retry without player id later
   sendCommand(PLAYER, opt->playerId);
 
   recvBuff = recvCommand(0); //
-  printServerResponse(recvBuff, NULL);
-
+  parseCommand(recvBuff, PLAYER);
   free(recvBuff);
+
+  game_info *gameInfo = getGameInfo();
+  printProlog(gameInfo, PRETTY);
+  freeGameInfo(gameInfo);
 }
--- performConnection.h
+++ performConnection.h
@@ -1,20 +1,113 @@
+/** @file performConnection.h
+ *  @brief Function to setup connection to Game Server.
+ *
+ *  This function implements the prolog phase of the protocol
+ *  and parse the server response. It also contains a function
+ *  for pretty printing.
+ *
+ *  @author Erik Kellenter
+ *  @bug recvCommand throws error when server sends 48 bytes after PLAYER
+ * COMMAND.
+ */
+
 #ifndef connectionUtil
 #define connectionUtil
 
+#include <stdbool.h>
+
 #define GAME_ID_SIZE 13
 
-typedef struct Opt
-{
-    char gameId[14];
-    char playerId[2];
+/**
+ * @brief Config struct
+ *
+ * Contains all the information passed as command-line arguments.
+ */
+typedef struct Opt {
+  char gameId[14];
+  char playerId[2];
 } opt_t;
 
-typedef enum COMMAND { VERSION, ID, PLAYER } COMMAND;
+/**
+ * @brief Config struct
+ *
+ * Describes a player sent by the server.
+ */
+typedef struct Player {
+  char playerName[30];
+  unsigned int playerId;
+  bool ready;
+} player_t;
 
+/**
+ * @brief Config struct
+ *
+ * Contains all relevant information of the prolog phase.
+ * Don't forget to free memory after usage.
+ */
+typedef struct GameInfo {
+  char serverVersion[10];
+  char gamekindName[20];
+  char gameName[30];
+  unsigned int total;
+  player_t **playerList;
+  char *raw;
+} game_info;
+
+/**
+ * @brief Config struct
+ *
+ * Contains all relevant commands for following functions.
+ */
+typedef enum COMMAND { RETURN, START, VERSION, ID, PLAYER } COMMAND;
+
+/**
+ * @brief Config struct
+ *
+ * Contains the flags for the function printProlog.
+ */
+typedef enum P_FLAG { PRETTY, DEBUG } P_FLAG;
+
+/** @brief implements prolog phase using passed Opt.
+ *
+ *  @param sock socket to connect with
+ *  @param opt options used for the connection
+ */
 void performConnection(int sock, opt_t *opt);
 
+/** @brief Sends command to server.
+ *
+ * The function parses and concatenates the command with the value
+ * passed to the function and afterwards send it to the server.
+ *
+ *  @param c enum COMMAND
+ *  @param value value that will be appended to command
+ */
 void sendCommand(COMMAND c, char *value);
 
-char *recvCommand(int lines);
+/** @brief Receives server response after sending a command.
+ *
+ * The function receives the number of lines passed as parameter.
+ * If lines is 0, then the function waits until an END command
+ * (e. g. ENDPLAYERS or ENDPIECELIST) is received. It returns
+ * a pointer to a dynamically allocated memory that must be freed.
+ *
+ *  @param lines expected lines the server will send
+ *  @return pointer to received data
+ */
+void *recvCommand(int lines);
+
+/** @brief getter for game_info struct.
+ *
+ * To avoid memory leaks, call freeGameInfo afterwards.
+ *
+ *  @return game_info struct
+ */
+game_info *getGameInfo();
+
+/** @brief frees memory allocated before.
+ *
+ *  @param gameInfo game_info struct that has been used before
+ */
+void freeGameInfo(game_info *gameInfo);
 
 #endif
