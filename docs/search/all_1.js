f4514192da4678ecf5b4d3c8a3a4b05e7afa9f72
Lukas Mahr <Lukas.Mahr@campus.lmu.de>
2021-09-30 12:26:51 +0200

    ADD docs

---
+++ docs/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['freegameinfo_2',['freeGameInfo',['../performConnection_8h.html#a6f72486ea2f588d520bfd79abf1c3e57',1,'performConnection.c']]]
+];
---
+++ docs/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['baschni_19',['Baschni',['../md_README.html',1,'']]],
+  ['bug_20list_20',['Bug List',['../bug.html',1,'']]]
+];
