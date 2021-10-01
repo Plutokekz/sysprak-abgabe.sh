3706c19da931cc0d6a257c5b47d238248ae6089c
Lukas Mahr <Lukas.Mahr@campus.lmu.de>
2021-10-02 01:04:27 +0200

    DOCS

--- docs/search/all_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['performconnection_8',['performConnection',['../performConnection_8h.html#a810e48631ff65df542158a0c7bfbca53',1,'performConnection.c']]],
-  ['performconnection_2eh_9',['performConnection.h',['../performConnection_8h.html',1,'']]],
-  ['player_10',['Player',['../structPlayer.html',1,'']]]
-];
