7117fe96a750813ec1b0b533ab1545475a7bca87
Lukas Mahr <Lukas.Mahr@campus.lmu.de>
2021-10-02 01:04:27 +0200

    DOCS

--- docs/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['gameinfo_3',['GameInfo',['../structGameInfo.html',1,'']]],
-  ['getgameinfo_4',['getGameInfo',['../performConnection_8h.html#a3422bd40d37dcdb1b25b25ec3567d64f',1,'performConnection.c']]]
-];
--- docs/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['freegameinfo_15',['freeGameInfo',['../performConnection_8h.html#a6f72486ea2f588d520bfd79abf1c3e57',1,'performConnection.c']]]
-];
